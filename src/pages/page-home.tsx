c65ca0230bc0733ddfe4537194efb2ba5be77757
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-08-22 14:51:26 -0300

    feat: add photo-list in page-home

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,24 +1,22 @@
 import Container from "../components/container";
-import PhotoWidget from "../context/photos/components/photo-widget";
+import PhotosList from "../context/photos/components/photo-list";
 
 export default function PageHome() {
   return (
     <>
       <Container>
-        <div className="grid grid-cols-4 gap-9">
-          <PhotoWidget
-            photo={{
-              id: "123",
-              title: "teste",
-              imageId: "portrait-tower.png",
-              albums: [
-                { id: "321", title: "Album 1" },
-                { id: "123", title: "Album 2" },
-                { id: "455", title: "Album 3" },
-              ],
-            }}
-          />
-        </div>
+        <PhotosList photos={[
+          {
+            id: "123",
+            title: "teste",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "321", title: "Album 1" },
+              { id: "123", title: "Album 2" },
+              { id: "455", title: "Album 3" },
+            ],
+          }
+        ]}  />
       </Container>
     </>
   );
