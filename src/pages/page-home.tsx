467b4d1d106bc2188a1169b6b0f2503b87c653c9
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-08-22 15:16:25 -0300

    feat: add albums-filter in page-home

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,10 +1,12 @@
 import Container from "../components/container";
+import AlbumsFilter from "../context/albums/models/components/albums-filter";
 import PhotosList from "../context/photos/components/photo-list";
 
 export default function PageHome() {
   return (
     <>
       <Container>
+        <AlbumsFilter albums={[{ id: "321", title: "Album 1" }]} className="mb-9" />
         <PhotosList photos={[
           {
             id: "123",
