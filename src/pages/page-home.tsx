a8df68229c344abf1497aec6148bf1ed4ad3434b
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-08-22 14:16:56 -0300

    feat: add photo-widget in page-home

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,9 +1,25 @@
-import Text from "../components/text";
+import Container from "../components/container";
+import PhotoWidget from "../context/photos/components/photo-widget";
 
 export default function PageHome() {
   return (
     <>
-      <Text variant="heading-medium">Home</Text>
+      <Container>
+        <div className="grid grid-cols-4 gap-9">
+          <PhotoWidget
+            photo={{
+              id: "123",
+              title: "teste",
+              imageId: "portrait-tower.png",
+              albums: [
+                { id: "321", title: "Album 1" },
+                { id: "123", title: "Album 2" },
+                { id: "455", title: "Album 3" },
+              ],
+            }}
+          />
+        </div>
+      </Container>
     </>
   );
 }
