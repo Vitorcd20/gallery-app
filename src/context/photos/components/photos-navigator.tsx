e7c1d0fe0b1248d00301f5b6c8e5c73a8056b1a5
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-08-23 11:40:34 -0300

    feat: add photos-navigator component

---
+++ src/context/photos/components/photos-navigator.tsx
@@ -0,0 +1,55 @@
+import ButtonIcon from "../../../components/button-icon";
+import Skeleton from "../../../components/skeleton";
+import ArrowLeft from "../../../assets/icons/chevron-left.svg?react";
+import ArrowRight from "../../../assets/icons/chevron-right.svg?react";
+import Button from "../../../components/button";
+import { useNavigate } from "react-router";
+import cx from "classnames";
+
+interface PhotosNavigatorProps extends React.ComponentProps<"div"> {
+  previousPhotoId?: string;
+  nextPhotoId?: string;
+  loading?: boolean;
+}
+
+export default function PhotosNavigator({
+  previousPhotoId,
+  nextPhotoId,
+  loading,
+  className,
+  ...props
+}: PhotosNavigatorProps) {
+  const navigate = useNavigate();
+
+  return (
+    <div className={cx("flex gap-2", className)} {...props}>
+      {!loading ? (
+        <>
+          <ButtonIcon
+            icon={ArrowLeft}
+            variant="secondary"
+            disabled={!previousPhotoId}
+            onClick={() => {
+              navigate(`/photos/${previousPhotoId}`);
+            }}
+          />
+          <Button
+            variant="secondary"
+            icon={ArrowRight}
+            disabled={!nextPhotoId}
+            onClick={() => {
+              navigate(`/photos/${nextPhotoId}`);
+            }}
+          >
+            Next Image
+          </Button>
+        </>
+      ) : (
+        <>
+          <Skeleton className="w-10 h-10" />
+          <Skeleton className="w-20 h-10" />
+        </>
+      )}
+    </div>
+  );
+}
