a11794e67dac3dbfedf3cb7929a706b8729785c6
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-08-23 12:18:38 -0300

    feat: add photo-new-dialog component

---
+++ src/context/photos/components/photo-new-dialog.tsx
@@ -0,0 +1,94 @@
+import { useForm } from "react-hook-form";
+import Alert from "../../../components/alert";
+import Button from "../../../components/button";
+import {
+  Dialog,
+  DialogBody,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "../../../components/dialog";
+import ImagePreview from "../../../components/image-preview";
+import InputSingleFile from "../../../components/input-single-file";
+import InputText from "../../../components/input-text";
+import Skeleton from "../../../components/skeleton";
+import Text from "../../../components/text";
+import type { Album } from "../../albums/models/album";
+
+interface PhotoNewDialogProps {
+  trigger: React.ReactNode;
+}
+
+export default function PhotoNewDialog({ trigger }: PhotoNewDialogProps) {
+  const form = useForm()
+
+  // mock for test
+  const isLoadingAlbum = false;
+
+  const albums: Album[] = [
+    { id: "321", title: "Album 1" },
+    { id: "123", title: "Album 2" },
+    { id: "455", title: "Album 3" },
+  ];
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>Upload a Photo</DialogHeader>
+
+        <DialogBody className="flex flex-col gap-5">
+          <InputText placeholder="Choose a title" maxLength={255} />
+
+          <Alert>
+            Maximum size: 50MB
+            <br />
+            You can select files in PNG, JPG or JPEG
+          </Alert>
+
+          <InputSingleFile
+            form={form}
+            allowedExtensions={["jpg", "jpeg", "png"]}
+            maxFileSizeInMB={50}
+            replaceBy={<ImagePreview className="w-full h-56" />}
+          />
+
+          <div className="space-y-3">
+            <Text variant="label-small">Select Albums</Text>
+
+            <div className="flex flex-wrap gap-3">
+
+            {!isLoadingAlbum &&
+              albums.length > 0 &&
+              albums.map((album) => (
+                <Button
+                  key={album.id}
+                  variant="ghost"
+                  size="sm"
+                  className="truncate"
+                >
+                  {album.title}
+                </Button>
+              ))}
+
+            {isLoadingAlbum &&
+              Array.from({ length: 5 }).map((_, index) => (
+                <Skeleton className="w-20 h-7" key={`album-loading-${index}`} />
+              ))}
+          </div>
+          </div>
+        </DialogBody>
+
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button variant="secondary">Cancel</Button>
+          </DialogClose>
+
+          <Button>Attach</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
