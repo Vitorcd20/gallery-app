e8b288f705db622163e9de50a3ea767c98a6a19c
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-08-26 20:25:49 -0300

    feat: add use-photos hooks in page-home

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -2,25 +2,21 @@ import Container from "../components/container";
 import AlbumsFilter from "../context/albums/components/albums-filter";
 import UseAlbums from "../context/albums/hooks/use-albums";
 import PhotosList from "../context/photos/components/photo-list";
+import UsePhotos from "../context/photos/hooks/use-photos";
 
 export default function PageHome() {
-  const {albums, isLoadingAlbums} = UseAlbums()
+  const { albums, isLoadingAlbums } = UseAlbums();
+  const { photos, isLoadingPhotos } = UsePhotos();
+
   return (
     <>
       <Container>
-        <AlbumsFilter albums={albums} loading={isLoadingAlbums} className="mb-9" />
-        <PhotosList photos={[
-          {
-            id: "123",
-            title: "teste",
-            imageId: "portrait-tower.png",
-            albums: [
-              { id: "321", title: "Album 1" },
-              { id: "123", title: "Album 2" },
-              { id: "455", title: "Album 3" },
-            ],
-          }
-        ]}  />
+        <AlbumsFilter
+          albums={albums}
+          loading={isLoadingAlbums}
+          className="mb-9"
+        />
+        <PhotosList photos={photos} loading={isLoadingPhotos} />
       </Container>
     </>
   );
