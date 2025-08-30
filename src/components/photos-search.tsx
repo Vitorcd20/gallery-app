5a1d83ae24dcc84b847ba99f3980c760457e86cd
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-08-30 14:06:02 -0300

    adding filter by text in the photo list

--- src/components/photos-search.tsx
+++ src/components/photos-search.tsx
@@ -2,16 +2,18 @@ import InputText from "./input-text";
 import SearchIcon from "../assets/icons/search.svg?react";
 import { useCallback, useState } from "react";
 import { debounce } from "../helpers/utils";
+import UsePhotos from "../context/photos/hooks/use-photos";
 
 export default function PhotosSearch() {
   const [inputValue, setInputValue] = useState("");
+  const { filters } = UsePhotos();
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedSetValue = useCallback(
     debounce((value: string) => {
-      console.log("value debounced:", value);
+      filters.setQ(value);
     }, 200),
-    []
+    [filters.setQ]
   );
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
