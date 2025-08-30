d28515c08b828e83a9ca363e8bd298cb3498e2c4
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-08-30 18:31:19 -0300

    new photo - clearing form when canceling

--- src/context/photos/components/photo-new-dialog.tsx
+++ src/context/photos/components/photo-new-dialog.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import Alert from "../../../components/alert";
 import Button from "../../../components/button";
@@ -17,89 +18,100 @@ import Skeleton from "../../../components/skeleton";
 import Text from "../../../components/text";
 import UseAlbums from "../../albums/hooks/use-albums";
 import { photoNewFormSchema, type PhotoNewFormSchema } from "../schemas";
-import {zodResolver} from '@hookform/resolvers/zod'
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface PhotoNewDialogProps {
   trigger: React.ReactNode;
 }
 
 export default function PhotoNewDialog({ trigger }: PhotoNewDialogProps) {
+  const [modalOpen, setModalOpen] = useState(false);
   const form = useForm<PhotoNewFormSchema>({
-    resolver: zodResolver(photoNewFormSchema)
-  })
+    resolver: zodResolver(photoNewFormSchema),
+  });
 
-  const {albums, isLoadingAlbums} = UseAlbums()
+  const { albums, isLoadingAlbums } = UseAlbums();
 
   const file = form.watch("file");
   const fileSource = file?.[0] ? URL.createObjectURL(file[0]) : undefined;
 
+  useEffect(() => {
+    if (!modalOpen) {
+      form.reset();
+    }
+  }, [modalOpen, form]);
+
   function handleSubmit(payload: PhotoNewFormSchema) {
-    console.log(payload)
+    console.log(payload);
   }
 
   return (
-    <Dialog>
+    <Dialog open={modalOpen} onOpenChange={setModalOpen}>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
       <DialogContent>
         <form onSubmit={form.handleSubmit(handleSubmit)}>
-        <DialogHeader>Upload a Photo</DialogHeader>
+          <DialogHeader>Upload a Photo</DialogHeader>
 
-        <DialogBody className="flex flex-col gap-5">
-          <InputText 
-          placeholder="Choose a title" 
-          maxLength={255} 
-          error={form.formState.errors.title?.message}
-          {...form.register("title")}
-          />
+          <DialogBody className="flex flex-col gap-5">
+            <InputText
+              placeholder="Choose a title"
+              maxLength={255}
+              error={form.formState.errors.title?.message}
+              {...form.register("title")}
+            />
 
-          <Alert>
-            Maximum size: 50MB
-            <br />
-            You can select files in PNG, JPG or JPEG
-          </Alert>
+            <Alert>
+              Maximum size: 50MB
+              <br />
+              You can select files in PNG, JPG or JPEG
+            </Alert>
 
-          <InputSingleFile
-            form={form}
-            allowedExtensions={["jpg", "jpeg", "png"]}
-            maxFileSizeInMB={50}
-            replaceBy={<ImagePreview src={fileSource} className="w-full h-56" />}
-            error={form.formState.errors.file?.message}
-            {...form.register("file")}
-          />
+            <InputSingleFile
+              form={form}
+              allowedExtensions={["jpg", "jpeg", "png"]}
+              maxFileSizeInMB={50}
+              replaceBy={
+                <ImagePreview src={fileSource} className="w-full h-56" />
+              }
+              error={form.formState.errors.file?.message}
+              {...form.register("file")}
+            />
 
-          <div className="space-y-3">
-            <Text variant="label-small">Select Albums</Text>
+            <div className="space-y-3">
+              <Text variant="label-small">Select Albums</Text>
 
-            <div className="flex flex-wrap gap-3">
+              <div className="flex flex-wrap gap-3">
+                {!isLoadingAlbums &&
+                  albums.length > 0 &&
+                  albums.map((album) => (
+                    <Button
+                      key={album.id}
+                      variant="ghost"
+                      size="sm"
+                      className="truncate"
+                    >
+                      {album.title}
+                    </Button>
+                  ))}
 
-            {!isLoadingAlbums &&
-              albums.length > 0 &&
-              albums.map((album) => (
-                <Button
-                  key={album.id}
-                  variant="ghost"
-                  size="sm"
-                  className="truncate"
-                >
-                  {album.title}
-                </Button>
-              ))}
+                {isLoadingAlbums &&
+                  Array.from({ length: 5 }).map((_, index) => (
+                    <Skeleton
+                      className="w-20 h-7"
+                      key={`album-loading-${index}`}
+                    />
+                  ))}
+              </div>
+            </div>
+          </DialogBody>
 
-            {isLoadingAlbums &&
-              Array.from({ length: 5 }).map((_, index) => (
-                <Skeleton className="w-20 h-7" key={`album-loading-${index}`} />
-              ))}
-          </div>
-          </div>
-        </DialogBody>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="secondary">Cancel</Button>
+            </DialogClose>
 
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="secondary">Cancel</Button>
-          </DialogClose>
-
-          <Button type="submit">Attach</Button>
-        </DialogFooter>
+            <Button type="submit">Attach</Button>
+          </DialogFooter>
         </form>
       </DialogContent>
     </Dialog>
