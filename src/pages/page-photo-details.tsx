81158d19e46f72fc28030b999aa7ceeec7c65c26
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-08-30 14:31:10 -0300

    returning detail from the photo

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -2,35 +2,32 @@ import { useParams } from "react-router";
 import Container from "../components/container";
 import PhotosNavigator from "../context/photos/components/photos-navigator";
 import Text from "../components/text";
-import type { Photo } from "../context/photos/models/photo";
 import Skeleton from "../components/skeleton";
 import ImagePreview from "../components/image-preview";
 import Button from "../components/button";
 import AlbumsListSelectable from "../context/albums/components/albums-list-selectable";
 import UseAlbums from "../context/albums/hooks/use-albums";
+import UsePhoto from "../context/photos/hooks/use-photo";
+import type { Photo } from "../context/photos/models/photo";
 
 export default function PagePhotoDetails() {
-  const {id} = useParams();
-  const {albums, isLoadingAlbums} = UseAlbums()
+  const { id } = useParams();
+  const { photo, isLoadingPhoto } = UsePhoto(id);
 
-  // for tests
-  const isLoadingPhoto = false;
-  const photo = {
-    id: "123",
-    imageId: "portrait-tower.png",
-    albums: [
-      { id: "321", title: "Album 1" },
-      { id: "123", title: "Album 2" },
-      { id: "455", title: "Album 3" },
-    ],
-  } as Photo;
+  const { albums, isLoadingAlbums } = UseAlbums();
+
+  if (!isLoadingPhoto && !photo) {
+    return <div>Photo not found!</div>;
+  }
 
   return (
     <>
       <Container>
         <header className="flex items-center justify-between gap-8 mb-8">
           {!isLoadingPhoto ? (
-            <Text as="h2" variant="heading-large">{photo?.title}</Text>
+            <Text as="h2" variant="heading-large">
+              {photo?.title}
+            </Text>
           ) : (
             <Skeleton className="w-48 h-48" />
           )}
@@ -42,7 +39,7 @@ export default function PagePhotoDetails() {
           <div className="space-y-3">
             {!isLoadingPhoto ? (
               <ImagePreview
-                src={`/images/${photo?.imageId}`}
+                src={`${import.meta.env.VITE_IMAGES_URL}/${photo?.imageId}`}
                 title={photo?.title}
                 imageClassName="h-[21rem]"
               />
@@ -57,12 +54,17 @@ export default function PagePhotoDetails() {
             )}
           </div>
 
-           <div className="py-3">
-            <Text as="h3" variant="heading-medium" className="mb-6">Albums</Text>
+          <div className="py-3">
+            <Text as="h3" variant="heading-medium" className="mb-6">
+              Albums
+            </Text>
 
-             <AlbumsListSelectable photo={photo} albums={albums} loading={isLoadingAlbums} />
-           </div>
-
+            <AlbumsListSelectable
+              photo={photo as Photo}
+              albums={albums}
+              loading={isLoadingAlbums}
+            />
+          </div>
         </div>
       </Container>
     </>
