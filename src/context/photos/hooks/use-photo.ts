ebc7f7e6defaf709d710e57a23081907b211fa0c
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-09-01 11:55:57 -0300

    add toaster

--- src/context/photos/hooks/use-photo.ts
+++ src/context/photos/hooks/use-photo.ts
@@ -2,6 +2,7 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { api, fetcher } from "../../../helpers/api";
 import type { Photo } from "../models/photo";
 import type { PhotoNewFormSchema } from "../schemas";
+import { toast } from "sonner";
 
 interface PhotoDetailResponse extends Photo {
   nextPhotoId?: string;
@@ -18,7 +19,7 @@ export default function UsePhoto(id?: string) {
    const queryClient = useQueryClient();
 
   async function createPhoto(payload: PhotoNewFormSchema) {
-    // eslint-disable-next-line no-useless-catch
+    
     try {
       const { data: photo } = await api.post<Photo>("/photos", {
         title: payload.title,
@@ -43,7 +44,10 @@ export default function UsePhoto(id?: string) {
       }
 
       queryClient.invalidateQueries({ queryKey: ["photos"] });
+
+      toast.success("Photo added successfully!")
     } catch (error) {
+      toast.error("Error creating photo")
       throw error;
     }
   }
