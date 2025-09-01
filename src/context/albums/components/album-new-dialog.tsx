e475494cd5a3ca6cd684e4306b97617ff103a516
Vitorcd20 <vitorvieeira12008@gmail.com>
2025-09-01 12:10:29 -0300

    new album -assembling schema and validating fields

--- src/context/albums/components/album-new-dialog.tsx
+++ src/context/albums/components/album-new-dialog.tsx
@@ -14,69 +14,103 @@ import SelectCheckboxIllustration from "../../../assets/images/select-checkbox.s
 import Button from "../../../components/button";
 import PhotoImageSelectable from "../../photos/components/photo-image-selectable";
 import UsePhotos from "../../photos/hooks/use-photos";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { albumNewFormSchema, type AlbumNewFormSchema } from "../schema";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 interface AlbumNewDialogProps {
   trigger: React.ReactNode;
 }
 
 export default function AlbumNewDialog({ trigger }: AlbumNewDialogProps) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const form = useForm<AlbumNewFormSchema>({
+    resolver: zodResolver(albumNewFormSchema),
+  });
   const { photos, isLoadingPhotos } = UsePhotos();
 
+  function handleTogglePhoto(selected: boolean, photoId: string) {
+    console.log(selected, photoId);
+  }
+
+  function handleSubmit(payload: AlbumNewFormSchema) {
+    console.log(payload);
+  }
+
+  useEffect(() => {
+    if (!modalOpen) {
+      form.reset();
+    }
+  }, [form, modalOpen]);
+
   return (
-    <Dialog>
+    <Dialog open={modalOpen} onOpenChange={setModalOpen}>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
       <DialogContent>
-        <DialogHeader>Create Album</DialogHeader>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <DialogHeader>Create Album</DialogHeader>
 
-        <DialogBody className="flex flex-col gap-5">
-          <InputText placeholder="Choose a title" />
+          <DialogBody className="flex flex-col gap-5">
+            <InputText
+              placeholder="Choose a title"
+              error={form.formState.errors.title?.message}
+              {...form.register("title")}
+            />
 
-          <div className="space-y-3">
-            <Text as="div" variant="label-small" className="mb-3">
-              Registered photos
-            </Text>
+            <div className="space-y-3">
+              <Text as="div" variant="label-small" className="mb-3">
+                Registered photos
+              </Text>
 
-            {!isLoadingPhotos && photos.length > 0 && (
-              <div className="flex flex-wrap gap-2">
-                {photos.map((photo) => (
-                  <PhotoImageSelectable
-                    key={photo.id}
-                    src={`${import.meta.env.VITE_IMAGES_URL}/${photo.imageId}`}
-                    title={photo.title}
-                    imageClassName="w-20 h-20"
-                  />
-                ))}
-              </div>
-            )}
+              {!isLoadingPhotos && photos.length > 0 && (
+                <div className="flex flex-wrap gap-2">
+                  {photos.map((photo) => (
+                    <PhotoImageSelectable
+                      key={photo.id}
+                      src={`${import.meta.env.VITE_IMAGES_URL}/${
+                        photo.imageId
+                      }`}
+                      title={photo.title}
+                      imageClassName="w-20 h-20"
+                      onSelectImage={(selected) =>
+                        handleTogglePhoto(selected, photo.id)
+                      }
+                    />
+                  ))}
+                </div>
+              )}
 
-            {isLoadingPhotos && (
-              <div className="flex flex-wrap gap-2">
-                {Array.from({ length: 4 }).map((_, index) => (
-                  <Skeleton
-                    key={`photo-loading-${index}`}
-                    className="w-20 h-20 rounded-lg"
-                  />
-                ))}
-              </div>
-            )}
+              {isLoadingPhotos && (
+                <div className="flex flex-wrap gap-2">
+                  {Array.from({ length: 4 }).map((_, index) => (
+                    <Skeleton
+                      key={`photo-loading-${index}`}
+                      className="w-20 h-20 rounded-lg"
+                    />
+                  ))}
+                </div>
+              )}
 
-            {!isLoadingPhotos && photos.length === 0 && (
-              <div className="w-full flex flex-col justify-center items-center gap-3">
-                <SelectCheckboxIllustration />
-                <Text variant="paragraph-medium" className="text-center">
-                  No photos available for selection
-                </Text>
-              </div>
-            )}
-          </div>
-        </DialogBody>
+              {!isLoadingPhotos && photos.length === 0 && (
+                <div className="w-full flex flex-col justify-center items-center gap-3">
+                  <SelectCheckboxIllustration />
+                  <Text variant="paragraph-medium" className="text-center">
+                    No photos available for selection
+                  </Text>
+                </div>
+              )}
+            </div>
+          </DialogBody>
 
-        <DialogFooter>
-          <DialogClose asChild>
-            <Button variant="secondary">Cancel</Button>
-          </DialogClose>
-          <Button>Create</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="secondary">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Create</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
